64479c04dc1ddb08f01c938173c2a59a4831b4c1
Eric Yang <esyang@umich.edu>
2018-11-15 19:03:50 -0500

    added audio, added Base Environment, added Sanctuary Demo

---
+++ Sanctuary Demo/Assets/Scripts/SceneCtrlJeanja.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrlJeanja : MonoBehaviour {
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void QuitApp()
+	{
+		Application.Quit();
+	}
+}
