84a019d8df56a4769b21a783a50093d44b3ffdfc
Eric Yang <esyang@umich.edu>
2018-11-26 14:15:46 -0500

    (1) Fixed camera location (2) fixed/added campfire toggle on/off interaction (3) fixed proper exit (4) fixed/added intra-application navigation NOTE: did not test on device as device no power

---
+++ Sanctuary Demo/Assets/Scripts/JeanjaOnClickSetActive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeanjaOnClickSetActive : MonoBehaviour {
+	public GameObject campfire;
+	public void OnClick() {
+		campfire.transform.GetChild(0).gameObject.SetActive(false);
+	}
+
+}
