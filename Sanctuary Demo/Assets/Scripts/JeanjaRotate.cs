797092c2ba5d78e291e7bd241866c2cc4434a79c
Eric Yang <esyang@umich.edu>
2018-12-05 20:50:26 -0500

    added earth scene

---
+++ Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeanjaRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// (x, y, z)
+		transform.Rotate(0, 0.05f, 0);
+	}
+}
