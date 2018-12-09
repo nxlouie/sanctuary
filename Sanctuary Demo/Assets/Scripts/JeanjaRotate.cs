4529ebee7278c95a8ff0d58d2729ad2cdfe9d06b
Eric Yang <esyang@umich.edu>
2018-12-09 17:43:08 -0500

    spin script

--- Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
+++ Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
@@ -16,20 +16,22 @@ public class JeanjaRotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (sw.Elapsed.TotalMilliseconds < 7600) {
-			transform.Rotate(0, 20f, 0);
+			transform.Rotate(0, 20f, 0); // (x, y, z)
 		}
 		else if (sw.Elapsed.TotalMilliseconds > 7600 && sw.Elapsed.TotalMilliseconds < 50000) {
-			transform.Rotate(0, 0.05f, 0);
+			transform.Rotate(0, 0.05f, 0); // (x, y, z)
 		}
 		else if (sw.Elapsed.TotalMilliseconds > 50000 && sw.Elapsed.TotalMilliseconds < 71600) {
-			transform.Rotate(0, 0.075f, 0);
+			transform.Rotate(0, 0.075f, 0); // (x, y, z)
 		}
 		else if (sw.Elapsed.TotalMilliseconds > 71600 && sw.Elapsed.TotalMilliseconds < 90600) {
-			transform.Rotate(0, 0.2f, 0);
+			transform.Rotate(0, 0.2f, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 90600) {
-			transform.Rotate(0, 18f, 0);
+		else if (sw.Elapsed.TotalMilliseconds > 90600 && sw.Elapsed.TotalMilliseconds < 129000) {
+			transform.Rotate(0, 18f, 0); // (x, y, z)
 		}
-		// (x, y, z)
+		else if (sw.Elapsed.TotalMilliseconds > 129000) {
+			transform.Rotate(0, 0.05f, 0); // (x, y, z)
+		}
 	}
 }
