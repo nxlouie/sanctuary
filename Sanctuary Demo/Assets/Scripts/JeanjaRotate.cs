16fcc7fde90b40ff856074362df62f6e428e0fbc
Eric Yang <esyang@umich.edu>
2018-12-09 17:29:44 -0500

    fixed space time

--- Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
+++ Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
@@ -15,20 +15,20 @@ public class JeanjaRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (sw.Elapsed.TotalMilliseconds < 5970) {
-			transform.Rotate(0, 26f, 0);
+		if (sw.Elapsed.TotalMilliseconds < 7600) {
+			transform.Rotate(0, 20f, 0);
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 5970 && sw.Elapsed.TotalMilliseconds < 50000) {
+		else if (sw.Elapsed.TotalMilliseconds > 7600 && sw.Elapsed.TotalMilliseconds < 50000) {
 			transform.Rotate(0, 0.05f, 0);
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 50000 && sw.Elapsed.TotalMilliseconds < 70000) {
+		else if (sw.Elapsed.TotalMilliseconds > 50000 && sw.Elapsed.TotalMilliseconds < 71600) {
 			transform.Rotate(0, 0.075f, 0);
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 70000 && sw.Elapsed.TotalMilliseconds < 89000) {
+		else if (sw.Elapsed.TotalMilliseconds > 71600 && sw.Elapsed.TotalMilliseconds < 90600) {
 			transform.Rotate(0, 0.2f, 0);
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 89000) {
-			transform.Rotate(0, 20f, 0);
+		else if (sw.Elapsed.TotalMilliseconds > 90600) {
+			transform.Rotate(0, 18f, 0);
 		}
 		// (x, y, z)
 	}
