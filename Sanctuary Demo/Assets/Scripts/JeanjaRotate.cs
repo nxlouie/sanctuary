d900e00131174867b2184aa146a788a45c9e56e6
Eric Yang <esyang@umich.edu>
2018-12-09 20:01:42 -0500

    slow down earth

--- Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
+++ Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class JeanjaRotate : MonoBehaviour {
     Stopwatch sw = Stopwatch.StartNew();
 
-	float rate = 0.05f;
+	float rate = 18f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +16,7 @@ public class JeanjaRotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (sw.Elapsed.TotalMilliseconds < 7600) {
-			transform.Rotate(0, 20f, 0); // (x, y, z)
+			transform.Rotate(0, rate, 0); // (x, y, z)
 		}
 		else if (sw.Elapsed.TotalMilliseconds > 7600 && sw.Elapsed.TotalMilliseconds < 50000) {
 			transform.Rotate(0, 0.05f, 0); // (x, y, z)
@@ -27,11 +27,14 @@ public class JeanjaRotate : MonoBehaviour {
 		else if (sw.Elapsed.TotalMilliseconds > 71600 && sw.Elapsed.TotalMilliseconds < 90600) {
 			transform.Rotate(0, 0.2f, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 90600 && sw.Elapsed.TotalMilliseconds < 129000) {
-			transform.Rotate(0, 18f, 0); // (x, y, z)
+		else if (sw.Elapsed.TotalMilliseconds > 90600 && sw.Elapsed.TotalMilliseconds < 124000) {
+			transform.Rotate(0, rate, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 129000) {
-			transform.Rotate(0, 0.05f, 0); // (x, y, z)
+		else if (sw.Elapsed.TotalMilliseconds > 124000) {
+			if (rate > 0.075f) {
+				rate = rate - 0.1f;
+			}
+			transform.Rotate(0, rate, 0); // (x, y, z)
 		}
 	}
 }
