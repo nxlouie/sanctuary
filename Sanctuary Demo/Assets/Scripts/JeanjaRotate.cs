881adb12cf386635ef801cdf713c85dcb4b80c67
Eric Yang <esyang@umich.edu>
2018-12-09 21:02:46 -0500

    fix earth timing

--- Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
+++ Sanctuary Demo/Assets/Scripts/JeanjaRotate.cs
@@ -15,26 +15,26 @@ public class JeanjaRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (sw.Elapsed.TotalMilliseconds < 5700) {
+		if (sw.Elapsed.TotalMilliseconds < 5950) {
 			if (rate < 0.5f) {
 				rate = rate + 0.05f;
 			}
 			transform.Rotate(0, rate, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 5700 && sw.Elapsed.TotalMilliseconds < 48100) {
+		else if (sw.Elapsed.TotalMilliseconds > 5950 && sw.Elapsed.TotalMilliseconds < 48350) {
 			transform.Rotate(0, 0.05f, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 48100 && sw.Elapsed.TotalMilliseconds < 69700) {
+		else if (sw.Elapsed.TotalMilliseconds > 48350 && sw.Elapsed.TotalMilliseconds < 69950) {
 			transform.Rotate(0, 0.075f, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 69700 && sw.Elapsed.TotalMilliseconds < 88700) {
+		else if (sw.Elapsed.TotalMilliseconds > 69950 && sw.Elapsed.TotalMilliseconds < 88950) {
 			transform.Rotate(0, 0.2f, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 88700 && sw.Elapsed.TotalMilliseconds < 122100) {
+		else if (sw.Elapsed.TotalMilliseconds > 88950 && sw.Elapsed.TotalMilliseconds < 122350) {
 			transform.Rotate(0, rate, 0); // (x, y, z)
 		}
-		else if (sw.Elapsed.TotalMilliseconds > 122100) {
-			if (rate > 0.075f) {
+		else if (sw.Elapsed.TotalMilliseconds > 122350) {
+			if (rate > 0.175f) {
 				rate = rate - 0.1f;
 			}
 			transform.Rotate(0, rate, 0); // (x, y, z)
